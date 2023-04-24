ec571387e54d7cbf35c58faf6e2e64f91d2ff0fe
Crowdbotics <engineering@crowdbotics.com>
2023-04-24 06:33:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fancy_shape_dev_78599/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fancy_shape_dev_78599;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "fancy_shape_dev_78599";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-shape-dev-78599",
+  url: "https://fancy-shape-dev-78599.botics.co",
+  api: "https://fancy-shape-dev-78599.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
